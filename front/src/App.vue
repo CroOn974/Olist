5dbe639e5c4b49e54b1ce0d89ae41dedd4635e38
Conisla <nthaothion@gmail.com>
2023-04-04 10:09:33 +0400

    my first commit

---
+++ front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <VuePlotly :data="data" :layout="layout" :display-mode-bar="false"></VuePlotly>
+</template>
+
+<script>
+import { VuePlotly } from 'vue3-plotly'
+
+export default {
+  name:'App',
+  components: {
+    VuePlotly
+  },
+  data() {
+    return {
+      data:[{
+        x: [1,2,3,4],
+        y: [10,15,13,17],
+        type:"bar"
+      }],
+      layout:{
+        title: "My graph"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
