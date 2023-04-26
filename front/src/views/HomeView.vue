7893be3c8f7ee7a2fcc7d928b0bdb5c5c6b7e215
Vacarme Guillaume <vacarme.guillaume@gmail.com>
2023-04-26 11:28:22 +0400

    affichage top 5 state et produit

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,6 +2,9 @@
 <div class="container h-screen">
   <NavBar/>
   <div class="flex bg-slate-100 " id="dashboard">
+    <select v-model="selectedYear" @change="updateDash()">
+      <option v-for="year in years" :value="year" :key="year">{{ year }}</option>
+    </select>
     <div class="mr-8 w-1/3 h-auto" id="colDough">
       <div class="">
         <DoughnutChart :option="option_pie_1"/>
@@ -63,6 +66,11 @@ export default {
   },
   data(){
     return{
+      selectedYear: '',
+      years: ['2017', '2018', '2019'],
+      limit: '5',
+      listState : [],
+      listProduct : [],
       option_pie_1: {
         title: {
           text: 'Top 5 Produits',
@@ -78,13 +86,7 @@ export default {
             type: 'pie',
             radius: ['40%', '70%'],
             center: ['50%', '50%'],
-            data: [
-              { value: 335, name: 'Direct' },
-              { value: 310, name: 'Email' },
-              { value: 234, name: 'Ad Networks' },
-              { value: 135, name: 'Video Ads' },
-              { value: 1548, name: 'Search Engines' },
-            ],
+            data: [],
             emphasis: {
               itemStyle: {
                 shadowBlur: 10,
@@ -111,11 +113,7 @@ export default {
             radius: ['40%', '70%'],
             center: ['50%', '50%'],
             data: [
-              { value: 335, name: 'Direct' },
-              { value: 310, name: 'Email' },
-              { value: 234, name: 'Ad Networks' },
-              { value: 135, name: 'Video Ads' },
-              { value: 1548, name: 'Search Engines' },
+              
             ],
             emphasis: {
               itemStyle: {
@@ -129,6 +127,60 @@ export default {
       },
       
     }
+  }
+  ,
+  /**
+   * Récupère des données
+   * 
+   * 
+   */
+  async created(){
+    
+  },
+  methods: {
+    updateDash(){
+      this.topState(this.selectedYear, this.limit)
+      this.topProduct(this.selectedYear, this.limit)
+    },
+    async topState(year, limit){
+      
+      var response = await fetch('http://localhost:8000/api/state-year/'+year+'/'+limit+'');
+      let data = await response.json();
+
+      this.option_pie_2.series[0].data = data.map((item) => {
+        return { value: item.turnover, name: item.state_name };
+      });
+
+      this.listState = data.map((item) => item.state_name);
+      this.evoState()
+
+    },
+    async topProduct(year, limit){
+
+      var response = await fetch('http://localhost:8000/api/product-year/'+year+'/'+limit+'');
+      let data = await response.json();
+      
+      this.option_pie_1.series[0].data = data.map((item) => {
+        return { value: item.turnover, name: item.product_id };
+      });
+
+      this.listProduct = data.map((item) => item.product_id);
+      this.evoProduct()
+    },
+    async evoState(){
+      const states = this.listState.join(',');
+      var response = await fetch('http://localhost:8000/api/states-evo/'+states+'/');
+      let data = await response.json();
+      console.log(data)
+    },    
+    async evoProduct(){
+      const product = this.listProduct.join(',');
+      var response = await fetch('http://localhost:8000/api/product-evo/'+product+'/');
+      let data = await response.json();
+      console.log(data)
+    }, 
+    
+
   }
 }
 </script>
